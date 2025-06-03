7a6f8bc75468f9469516744bb23471bd6cc609a2
Huzaifa Waleed <huzaifainaam225@gmail.com>
2025-06-03 19:35:01 +0500

    ...

---
+++ JS/day1.js
@@ -0,0 +1,4 @@
+// alert("Hello world");
+console.log("code is running..")
+ console.log("code is running again...")
+ console.log("code is running once again.....")
---
+++ JS/stringsday3.js
@@ -0,0 +1,24 @@
+ // Single Quote
+ let msg1 = 'Hello\n';
+// // Double Quote
+ let msg2 = "World\n";
+ // Backticks
+let msg3 = `Hello World\n`;
+// String + String = Concatination
+ let msg4 = `Hello World` + "Hello Duniya";
+
+// String + Number = Type Cohersion In this when the converts into string is called type cohersion
+let msg5 = `Hello World` + "100";
+
+// String + (Number + Number)
+let msg6 = `Hello World` + (100 + 100);
+
+ let sum = '3' + '3\n';
+  let add =
+  5+5; 
+ let me = "I'm a boy\n" 
+
+
+// console.log("\n",add);
+console.log(msg6);
+
