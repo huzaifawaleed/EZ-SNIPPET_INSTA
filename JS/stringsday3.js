d6f546ace2f044f7b4dc0d1e2942ac2481b215b9
Huzaifa Waleed <huzaifainaam225@gmail.com>
2025-06-03 19:42:53 +0500

    ..

--- JS/stringsday3.js
+++ JS/stringsday3.js
@@ -5,20 +5,21 @@
  // Backticks
 let msg3 = `Hello World\n`;
 // String + String = Concatination
- let msg4 = `Hello World` + "Hello Duniya";
+ let msg4 = `Hello World` + "Hello Duniya\n";
 
 // String + Number = Type Cohersion In this when the converts into string is called type cohersion
-let msg5 = `Hello World` + "100";
+let msg5 = `Hello World` + "100\n";
 
 // String + (Number + Number)
 let msg6 = `Hello World` + (100 + 100);
 
  let sum = '3' + '3\n';
+
   let add =
   5+5; 
  let me = "I'm a boy\n" 
 
 
-// console.log("\n",add);
-console.log(msg6);
+console.log("\n",add);
+console.log(msg1, msg2, msg3, msg4, msg5, msg6, sum, me);
 
