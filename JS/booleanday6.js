1a40d1812c4accbe5b1c8ca1839afbe8a2a1116b
Huzaifa Waleed <huzaifainaam225@gmail.com>
2025-06-07 15:39:02 +0500

    ..

---
+++ JS/booleanday6.js
@@ -0,0 +1,20 @@
+ console.log(10<100); /*true*/
+console.log(10>100);  /*false*/
+
+console.log(10>=10);   /*true*/
+console.log(10<=10);    /*true*/
+
+
+console.log(10 == 10);/*true*/   /*== checks the Equality*/
+console.log(10 == 9);  /*false*/   /*== checks the Equality*/
+
+
+console.log(10===10);  /*true*/       /*== checks the Equality and Type*/
+
+console.log(10==='10');  /*false*/     /*== checks the Equality and Type*/
+
+console.log(10 !== 10);  /*false*/
+console.log(10 !== 8);  /*true*/
+
+console.log(typeof 3);   /*Number*/
+console.log(typeof "Hello");   /*String*/
---
+++ JS/variablesday5.js
@@ -0,0 +1,17 @@
+let Name = "Huzaifa Waleed";
+let gender = "Male" + 18;
+let phoneNum = 923180600328;
+let email = "Huzaifainaam225@gmail.com";
+Name = "Usama Inam";
+let sum = 3 + 8;
+sum = 8 + 8;
+
+let genderr = "Male";
+genderr = "Female"
+
+console.log (Name);
+console.log (gender);
+console.log (phoneNum);
+console.log (email);
+console.log (sum);
+console.log (genderr);
