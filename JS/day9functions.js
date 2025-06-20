f70ecad2de701ab6322cc1e8f9e7a3ddc94eb2d9
Huzaifa Waleed <huzaifainaam225@gmail.com>
2025-06-21 01:38:21 +0500

    functions

---
+++ JS/day9functions.js
@@ -0,0 +1,15 @@
+// function temp(){
+//     console.log ("Huzaifa");
+//     console.log (20);    
+// }
+// temp();
+
+
+function add(){
+    let a = 20;
+    let b = 30;
+    return a + b;
+}
+
+// let sum = add(); // get output ith this.
+console.log(add());  // or with this same.
